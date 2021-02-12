fa95f2531c22914a9ad1b2cb2c71228d79c2497d
Mayank Mane <72073209+Myank-Mane@users.noreply.github.com>
2021-02-12 14:23:50 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDmaiRlXYbexRq5oHxL20iBVq0vZoIur6k",
+    authDomain: "kwitter-b5308.firebaseapp.com",
+    databaseURL: "https://kwitter-b5308-default-rtdb.firebaseio.com",
+    projectId: "kwitter-b5308",
+    storageBucket: "kwitter-b5308.appspot.com",
+    messagingSenderId: "871718366750",
+    appId: "1:871718366750:web:be95a033d477659a85480f"
+};
+firebase.initializeApp(firebaseConfig);
